2aaf05fab6228042aaadcc7c1baa922007c0ab3c
Songhao Li <songhao.li@hotmail.com>
2024-04-18 22:16:44 -0400

    Extracted Canvas

---
+++ src/app/components/Canvas.tsx
@@ -0,0 +1,111 @@
+
+
+import { useRef } from "react";
+import { useEffect } from "react";
+import { Polygon } from "@/app/logics/polygon";
+import { Vector2 } from '@/app/logics/vector2';
+import { Line } from "@/app/logics/line";
+import { RotationalCentersAnalyzer } from "@/app/logics/rotation-center-arc";
+import { RotationCenterFinder } from "@/app/logics/rotation-center-finder";
+import { PolygonConstructionData } from "@/app/logics/polygon-constructor";
+export default function Canvas(props: { polygon: Polygon, comparisonRotationalCenters: Vector2[], polygonData: PolygonConstructionData }) {
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // update the canvas when the polygon changes
+  useEffect(() => {
+
+    function drawVertices(ctx: CanvasRenderingContext2D) {
+      props.polygon.vertices.forEach((vertex, index) => {
+        ctx.beginPath();
+        ctx.arc(vertex.x, vertex.y, 7, 0, 2 * Math.PI);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(vertex.x, vertex.y, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "grey";
+        ctx.fill();
+      });
+    }
+
+
+    function drawLine(ctx: CanvasRenderingContext2D, line: Line) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.moveTo(line.start.x, line.start.y);
+      ctx.lineTo(line.end.x, line.end.y);
+      ctx.strokeStyle = 'black';
+      ctx.stroke();
+    }
+
+    function drawLineLabel(ctx: CanvasRenderingContext2D, line: Line) {
+      ctx.beginPath();
+      const middleX = (line.start.x + line.end.x) / 2;
+      const middleY = (line.start.y + line.end.y) / 2;
+      const dx = line.end.x - line.start.x;
+      const dy = line.end.y - line.start.y;
+      const offsetX = -dy;
+      const offsetY = dx;
+      const offsetMagnitude = 10; // adjust the offset magnitude as needed
+      const normalizedOffsetX = offsetX / Math.sqrt(offsetX * offsetX + offsetY * offsetY);
+      const normalizedOffsetY = offsetY / Math.sqrt(offsetX * offsetX + offsetY * offsetY);
+      const offsetPointX = middleX + normalizedOffsetX * offsetMagnitude;
+      const offsetPointY = middleY + normalizedOffsetY * offsetMagnitude;
+      ctx.font = '15px Arial';
+      ctx.fillStyle = 'black';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(`${line.getLength()}`, offsetPointX, offsetPointY);
+    }
+
+    if (props.polygon.vertices.length < 4) {
+      return;
+    }
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d') as CanvasRenderingContext2D;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    props.polygon.lines.forEach((line, index) => {
+      // draw the line
+      drawLine(ctx, line);
+      // draw the index of the line
+      drawLineLabel(ctx, line);
+    });
+    drawVertices(ctx);
+
+    // draw the rotational centers
+    if (props.polygon.vertices.length === 4) {
+      let centers = new RotationalCentersAnalyzer(props.polygonData).findRotationalCenters();
+      centers.forEach((center, index) => {
+        ctx.beginPath();
+        ctx.arc(center.x, center.y, 1, 0, 2 * Math.PI);
+        ctx.fillStyle = "red"
+        ctx.fill();
+      });
+    }
+
+    // draw the current rotational center
+    if (props.polygon.vertices.length === 4) {
+      const rotationalCenter = new RotationCenterFinder(props.polygon).getRotationalCenter();
+      ctx.beginPath();
+      ctx.arc(rotationalCenter.x, rotationalCenter.y, 5, 0, 2 * Math.PI);
+      ctx.fillStyle = "blue"
+      ctx.fill();
+    }
+
+    // draw the comparison rotational centers
+    props.comparisonRotationalCenters.forEach((center, index) => {
+      ctx.beginPath();
+      ctx.arc(center.x, center.y, 1, 0, 2 * Math.PI);
+      ctx.fillStyle = "green"
+      ctx.fill();
+    });
+
+  }, [props.polygon, props.comparisonRotationalCenters, props.polygonData]);
+  return (
+    <canvas className="w-full self-center h-full" id="myCanvas" width={800} height={800} ref={canvasRef} ></canvas>);
+
+
+}
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,18 @@
 'use client';
 
-import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Vector2 } from '@/app/logics/vector2';
 import { PolygonConstructionData, PolygonConstructor } from '@/app/logics/polygon-constructor';
-import { Polygon } from "./logics/polygon";
+import { Polygon } from "@/app/logics/polygon";
 import { Modal, TextField } from "@mui/material";
-import { RotationalCentersAnalyzer } from "./logics/rotation-center-arc";
-import { RotationCenterFinder } from "./logics/rotation-center-finder";
 import { HandsOffDistance } from "./logics/similarity-calculator";
 import Settings from "./settings/page";
 import Box from '@mui/material/Box';
 import PolygonInfoPanel from "./components/PolygonInfo";
-import { Line } from "./logics/line";
 
 
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [polygon, setPolygon] = useState<Polygon>(new Polygon([]));
   const [polygonData, setPolygonData] = useState<PolygonConstructionData>(
     new PolygonConstructionData()
@@ -33,52 +28,6 @@ export default function Home() {
     setPolygon((c) => polygonConstructor.constructPolygon());
   }, [polygonData]);
 
-  // update the canvas when the polygon changes
-  useEffect(() => {
-    if (polygon.vertices.length < 4) {
-      return;
-    }
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext('2d') as CanvasRenderingContext2D;
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    polygon.lines.forEach((line, index) => {
-      // draw the line
-      drawLine(ctx, line);
-      // draw the index of the line
-      drawLineLabel(ctx, line);
-    });
-    drawVertices(ctx);
-
-    // draw the rotational centers
-    if (polygon.vertices.length === 4) {
-      let centers = new RotationalCentersAnalyzer(polygonData).findRotationalCenters();
-      centers.forEach((center, index) => {
-        ctx.beginPath();
-        ctx.arc(center.x, center.y, 1, 0, 2 * Math.PI);
-        ctx.fillStyle = "red"
-        ctx.fill();
-      });
-    }
-
-    // draw the current rotational center
-    if (polygon.vertices.length === 4) {
-      const rotationalCenter = new RotationCenterFinder(polygon).getRotationalCenter();
-      ctx.beginPath();
-      ctx.arc(rotationalCenter.x, rotationalCenter.y, 5, 0, 2 * Math.PI);
-      ctx.fillStyle = "blue"
-      ctx.fill();
-    }
-
-    // draw the comparison rotational centers
-    comparisonRotationalCenters.forEach((center, index) => {
-      ctx.beginPath();
-      ctx.arc(center.x, center.y, 1, 0, 2 * Math.PI);
-      ctx.fillStyle = "green"
-      ctx.fill();
-    });
-
-  }, [polygon, comparisonRotationalCenters]);
   return (
     <main className="w-full bg-green-300 items-center mx-auto columns-1 p-6">
       <div className="items-end py-2" >
@@ -87,7 +36,7 @@ export default function Home() {
         </button>
       </div>
       <div className="columns-2">
-        <canvas className="w-full self-center h-full" id="myCanvas" width={800} height={800} ref={canvasRef} ></canvas>
+
         <PolygonInfoPanel data={polygonData} setData={setPolygonData} />
 
         <div>
@@ -124,48 +73,4 @@ export default function Home() {
     </main >
   );
 
-  function drawVertices(ctx: CanvasRenderingContext2D) {
-    polygon.vertices.forEach((vertex, index) => {
-      ctx.beginPath();
-      ctx.arc(vertex.x, vertex.y, 7, 0, 2 * Math.PI);
-      ctx.fillStyle = "black";
-      ctx.fill();
-      ctx.beginPath();
-      ctx.arc(vertex.x, vertex.y, 5, 0, 2 * Math.PI);
-      ctx.fillStyle = "grey";
-      ctx.fill();
-    });
-  }
-
-
-  function drawLine(ctx: CanvasRenderingContext2D, line: Line) {
-    ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.moveTo(line.start.x, line.start.y);
-    ctx.lineTo(line.end.x, line.end.y);
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-  }
-
-  function drawLineLabel(ctx: CanvasRenderingContext2D, line: Line) {
-    ctx.beginPath();
-    const middleX = (line.start.x + line.end.x) / 2;
-    const middleY = (line.start.y + line.end.y) / 2;
-    const dx = line.end.x - line.start.x;
-    const dy = line.end.y - line.start.y;
-    const offsetX = -dy;
-    const offsetY = dx;
-    const offsetMagnitude = 10; // adjust the offset magnitude as needed
-    const normalizedOffsetX = offsetX / Math.sqrt(offsetX * offsetX + offsetY * offsetY);
-    const normalizedOffsetY = offsetY / Math.sqrt(offsetX * offsetX + offsetY * offsetY);
-    const offsetPointX = middleX + normalizedOffsetX * offsetMagnitude;
-    const offsetPointY = middleY + normalizedOffsetY * offsetMagnitude;
-    ctx.font = '15px Arial';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(`${line.getLength()}`, offsetPointX, offsetPointY);
-  }
 }
-
-
