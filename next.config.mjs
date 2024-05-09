a7beea0438da94f65d8357876432fc7a70c15dbf
Songhao Li <songhao.li@hotmail.com>
2024-05-08 20:14:34 -0600

    added fabricjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@
 const nextConfig = {
   output: 'export',
   crossOrigin: 'anonymous',
-
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
 };
 
 export default nextConfig;
