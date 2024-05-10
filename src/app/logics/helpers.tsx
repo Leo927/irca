cd27b8b70831ce71c8647c313ece9b27be279796
Songhao Li <songhao.li@hotmail.com>
2024-05-09 19:41:08 -0600

    refactor: Fix incorrect order of translated edge names in helpers.tsx

--- src/app/logics/helpers.tsx
+++ src/app/logics/helpers.tsx
@@ -2,8 +2,8 @@ export function translateEdgeName(index: number): string {
     switch (index) {
         case 0: return '下连杆';
         case 1: return '后连杆';
-        case 2: return '前连杆';
-        case 3: return '上连杆';
+        case 2: return '上连杆';
+        case 3: return '前连杆';
         default: return '';
     }
 }
