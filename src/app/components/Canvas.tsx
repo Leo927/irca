7ffc3300141d5cf133f80fa0bdd7277a19fc7358
Songhao Li <songhao.li@hotmail.com>
2024-05-11 12:32:05 -0600

    Added zoom

--- src/app/components/Canvas.tsx
+++ src/app/components/Canvas.tsx
@@ -1,17 +1,16 @@
-
-
 import { useRef } from "react";
 import { useEffect, useState } from "react";
 import { HistoricalPolygonData } from "@/context/polygondatas";
+import { IconButton } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import * as fabric from 'fabric';
 
 import PolygonDrawer from "@/app/logics/polygondrawer";
 
 export default function Canvas(props: { polygonDatas: HistoricalPolygonData[]; }) {
-
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [ctx, setCtx] = useState<fabric.Canvas | null>(null);
-
 
   // update the canvas when the polygon changes
   useEffect(() => {
@@ -37,8 +36,29 @@ export default function Canvas(props: { polygonDatas: HistoricalPolygonData[]; }
     });
   }, [ctx, props.polygonDatas]);
 
+  const handleZoomIn = () => {
+    if (ctx !== null) {
+      ctx.setZoom(ctx.getZoom() * 1.1);
+    }
+  };
+
+  const handleZoomOut = () => {
+    if (ctx !== null) {
+      ctx.setZoom(ctx.getZoom() / 1.1);
+    }
+  };
+
   return (
-    <canvas className="self-center h-full" id="myCanvas" width="100%" height={400} ref={canvasRef} ></canvas>);
-
-
+    <div style={{ position: "relative" }}>
+      <canvas className="self-center h-full" id="myCanvas" width="100%" height={400} ref={canvasRef}></canvas>
+      <div style={{ position: "absolute", top: 0, right: 0 }}>
+        <IconButton onClick={handleZoomIn}>
+          <AddIcon />
+        </IconButton>
+        <IconButton onClick={handleZoomOut}>
+          <RemoveIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
